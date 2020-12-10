83d7467f264bd7ab81a240bd426ebeb0b4551ddb
meghanapulavarthy <68858341+meghanapulavarthy@users.noreply.github.com>
2020-12-11 00:42:07 +0530

    answer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1>hello</h1>
+      <form>
+        <input data-testid="name">Name</input>
+        <input data-testid = 'email'> Email address</input>
+        <input data-testid = 'gender'>Gender</input>
+        <input data-testid = 'phoneNumber'>Phone Number</input>
+        <input data-testid = 'password' type="password">password</input>
+        <button data-testid = 'submit'>submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
